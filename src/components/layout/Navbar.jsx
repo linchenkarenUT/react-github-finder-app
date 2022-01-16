6ea91d021480bdecc975f5d302d577efd4e825fc
Lin Chen <linchenkaren@gmail.com>
2022-01-16 11:26:17 -0600

    initial commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,45 @@
+import {FaGithub} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import {useContext} from 'react'
+import GithubContext from '../../context/github/GithubContext'
+
+function Navbar({title}) {
+
+    const { dispatch } = useContext(GithubContext)
+
+    return (
+        <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+            <div className="container mx-auto">
+                <div className="flex-none px-2 mx-2">
+                    <FaGithub className='inline pr-2 text-3xl' />
+                    <Link to='/' className='text-lg font-bold align-middle' onClick={()=>dispatch({type: 'CLEAR_USERS'})}>
+                        {title}
+                    </Link>
+                </div>
+                <div className="flex-1 px-2 mx-2">
+                    <div className="flex justify-end">
+                        <Link to='/' className='btn btn-ghost btn-sm rounded-btn' onClick={()=>dispatch({type: 'CLEAR_USERS'})}>
+                            Home
+                        </Link>
+                        <Link to='/about' className='btn btn-ghost btn-sm rounded-btn' >
+                            
+                            About
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+Navbar.defaultProps = {
+    title: 'Github Finder'
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string
+}
+
+export default Navbar
